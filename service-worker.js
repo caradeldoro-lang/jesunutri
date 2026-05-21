e29d2ccfd5c2c25d204522a4be75381614634e93
caradeldoro-lang <caradeldoro@gmail.com>
2026-05-21 03:30:16 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jesunutri-pwa-v2";
+const CACHE_NAME = "jesunutri-pwa-v3";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
@@ -61,4 +61,3 @@ self.addEventListener("fetch", (event) => {
     event.respondWith(cacheFirst(event.request));
   }
 });
-
